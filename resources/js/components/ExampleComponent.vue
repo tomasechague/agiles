94cab6b9b9a59b9299e1c6e5e3c72bfee1c86711
tomas echague <tomiechague@gmail.com>
2019-10-14 18:02:58 -0300

    Cambio titulo header busqueda profesores

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Bienvenido</div>
+                    <div class="card-header">Busqueda de Profesores</div>
                     <div class="card-body">
                         <form>
                           <div class="form-group">
